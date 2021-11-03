d3cc9b18536b7bb693595b3281581063fe603783
Sigurdmazanti <sigmn@live.dk>
2021-11-03 14:15:09 +0100

    Data fetch, search function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,55 @@
-fetch("./js/data.json")
-.then(response => {
-   return response.json();
-})
-.then(data => console.log(data));
+"use strict";
+
+// Global data variables - empty arrays for holding data from JSON
+let _visitDenmarkData = [];
+let _visitDenmarkCategories = [];
+
+// All the data
+async function fetchAllData() {
+    const response = await fetch('json/data.json');
+    const data = await response.json();
+    _visitDenmarkData = data;
+    console.log(_visitDenmarkData);
+    appendEvents(_visitDenmarkData);
+}
+fetchAllData();
+
+// All the category data
+async function fetchCategories() {
+    const response = await fetch('json/categories.json');
+    const data = await response.json();
+    _visitDenmarkCategories = data;
+    console.log(_visitDenmarkCategories);
+}
+fetchCategories();
+
+// Appending to DOM
+function appendEvents(events) {
+    let htmlEvents = "";
+    for (let event of events) {
+        htmlEvents += /*html*/ `
+      <article>
+        <div>
+          <p>${event.Name}</p>
+        </div>
+      </article>  
+      `;
+    }
+    document.querySelector("#event").innerHTML = htmlEvents;
+}
+
+// Search for name or category (cafe, bar)
+function search(searchValue) {
+    searchValue = searchValue.toLowerCase();
+    let results = [];
+    for (let searchedEvent of _visitDenmarkData) {
+        let name = searchedEvent.Name.toLowerCase();
+        let category = searchedEvent.Category.Name.toLowerCase();
+        // cafés fra JSON har en special karakter, det her 
+        let categorySpecial = category.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+            if (name.includes(searchValue) || categorySpecial.includes(searchValue)) {
+                results.push(searchedEvent);
+            }
+        }
+    appendEvents(results);
+}
